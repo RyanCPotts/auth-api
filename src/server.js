6d138a02d8690d3f6d4a6a786cd1df34f9d2f6be
RYAN <ryancpotts@gmail.com>
2024-07-10 17:02:32 -0500

    auth api nearly good

--- src/server.js
+++ src/server.js
@@ -12,7 +12,11 @@ const authRoutes = require('./routes/routes.js')
 const v1Routes = require('./routes/v1.js');
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(authRoutes)
